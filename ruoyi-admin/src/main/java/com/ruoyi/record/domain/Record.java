cfef88de809851184404fead267a8b975b49474f
szf <2053858365@qq.com>
2024-05-08 15:50:56 +0800

    record和store生成代码

---
+++ ruoyi-admin/src/main/java/com/ruoyi/record/domain/Record.java
@@ -0,0 +1,196 @@
+package com.ruoyi.record.domain;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
+
+/**
+ * 我的订单对象 record
+ * 
+ * @author ruoyi
+ * @date 2024-05-07
+ */
+public class Record extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 订单号 */
+    @Excel(name = "订单号")
+    private Long recordId;
+
+    /** 客户号 */
+    @Excel(name = "客户号")
+    private String uId;
+
+    /** 商店号 */
+    @Excel(name = "商店号")
+    private String sId;
+
+    /** 总价 */
+    @Excel(name = "总价")
+    private Long allItemPrice;
+
+    /** 骑手号 */
+    @Excel(name = "骑手号")
+    private String riderId;
+
+    /** 派送费 */
+    @Excel(name = "派送费")
+    private Long deliveryPrice;
+
+    /** 订单状态 */
+    @Excel(name = "订单状态")
+    private Long status;
+
+    /** 商店地址 */
+    @Excel(name = "商店地址")
+    private String srcPosition;
+
+    /** 送达地址 */
+    @Excel(name = "送达地址")
+    private String destPosition;
+
+    /** 下单时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Excel(name = "下单时间", width = 30, dateFormat = "yyyy-MM-dd")
+    private Date srcTime;
+
+    /** 送达时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Excel(name = "送达时间", width = 30, dateFormat = "yyyy-MM-dd")
+    private Date destTime;
+
+    /** 订单类型 */
+    @Excel(name = "订单类型")
+    private Long type;
+
+    public void setRecordId(Long recordId) 
+    {
+        this.recordId = recordId;
+    }
+
+    public Long getRecordId() 
+    {
+        return recordId;
+    }
+    public void setuId(String uId) 
+    {
+        this.uId = uId;
+    }
+
+    public String getuId() 
+    {
+        return uId;
+    }
+    public void setsId(String sId) 
+    {
+        this.sId = sId;
+    }
+
+    public String getsId() 
+    {
+        return sId;
+    }
+    public void setAllItemPrice(Long allItemPrice) 
+    {
+        this.allItemPrice = allItemPrice;
+    }
+
+    public Long getAllItemPrice() 
+    {
+        return allItemPrice;
+    }
+    public void setRiderId(String riderId) 
+    {
+        this.riderId = riderId;
+    }
+
+    public String getRiderId() 
+    {
+        return riderId;
+    }
+    public void setDeliveryPrice(Long deliveryPrice) 
+    {
+        this.deliveryPrice = deliveryPrice;
+    }
+
+    public Long getDeliveryPrice() 
+    {
+        return deliveryPrice;
+    }
+    public void setStatus(Long status) 
+    {
+        this.status = status;
+    }
+
+    public Long getStatus() 
+    {
+        return status;
+    }
+    public void setSrcPosition(String srcPosition) 
+    {
+        this.srcPosition = srcPosition;
+    }
+
+    public String getSrcPosition() 
+    {
+        return srcPosition;
+    }
+    public void setDestPosition(String destPosition) 
+    {
+        this.destPosition = destPosition;
+    }
+
+    public String getDestPosition() 
+    {
+        return destPosition;
+    }
+    public void setSrcTime(Date srcTime) 
+    {
+        this.srcTime = srcTime;
+    }
+
+    public Date getSrcTime() 
+    {
+        return srcTime;
+    }
+    public void setDestTime(Date destTime) 
+    {
+        this.destTime = destTime;
+    }
+
+    public Date getDestTime() 
+    {
+        return destTime;
+    }
+    public void setType(Long type) 
+    {
+        this.type = type;
+    }
+
+    public Long getType() 
+    {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("recordId", getRecordId())
+            .append("uId", getuId())
+            .append("sId", getsId())
+            .append("allItemPrice", getAllItemPrice())
+            .append("riderId", getRiderId())
+            .append("deliveryPrice", getDeliveryPrice())
+            .append("status", getStatus())
+            .append("srcPosition", getSrcPosition())
+            .append("destPosition", getDestPosition())
+            .append("srcTime", getSrcTime())
+            .append("destTime", getDestTime())
+            .append("type", getType())
+            .toString();
+    }
+}
---
+++ ruoyi-admin/src/main/java/com/ruoyi/record/service/impl/RecordServiceImpl.java
@@ -0,0 +1,93 @@
+package com.ruoyi.record.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ruoyi.record.mapper.RecordMapper;
+import com.ruoyi.record.domain.Record;
+import com.ruoyi.record.service.IRecordService;
+
+/**
+ * 我的订单Service业务层处理
+ * 
+ * @author ruoyi
+ * @date 2024-05-07
+ */
+@Service
+public class RecordServiceImpl implements IRecordService 
+{
+    @Autowired
+    private RecordMapper recordMapper;
+
+    /**
+     * 查询我的订单
+     * 
+     * @param recordId 我的订单主键
+     * @return 我的订单
+     */
+    @Override
+    public Record selectRecordByRecordId(Long recordId)
+    {
+        return recordMapper.selectRecordByRecordId(recordId);
+    }
+
+    /**
+     * 查询我的订单列表
+     * 
+     * @param record 我的订单
+     * @return 我的订单
+     */
+    @Override
+    public List<Record> selectRecordList(Record record)
+    {
+        return recordMapper.selectRecordList(record);
+    }
+
+    /**
+     * 新增我的订单
+     * 
+     * @param record 我的订单
+     * @return 结果
+     */
+    @Override
+    public int insertRecord(Record record)
+    {
+        return recordMapper.insertRecord(record);
+    }
+
+    /**
+     * 修改我的订单
+     * 
+     * @param record 我的订单
+     * @return 结果
+     */
+    @Override
+    public int updateRecord(Record record)
+    {
+        return recordMapper.updateRecord(record);
+    }
+
+    /**
+     * 批量删除我的订单
+     * 
+     * @param recordIds 需要删除的我的订单主键
+     * @return 结果
+     */
+    @Override
+    public int deleteRecordByRecordIds(Long[] recordIds)
+    {
+        return recordMapper.deleteRecordByRecordIds(recordIds);
+    }
+
+    /**
+     * 删除我的订单信息
+     * 
+     * @param recordId 我的订单主键
+     * @return 结果
+     */
+    @Override
+    public int deleteRecordByRecordId(Long recordId)
+    {
+        return recordMapper.deleteRecordByRecordId(recordId);
+    }
+}
